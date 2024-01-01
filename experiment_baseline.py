8affa63961616afea04bd4429940c80e42574afe
minkowski0125 <zhengwd23@mails.tsinghua.edu.cn>
2024-01-01 15:16:36 +0000

    add ridge and tree

---
+++ experiment_baseline.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import logging
+import numpy as np
+import torch
+torch.set_num_threads(2)
+from torch.utils.data.dataset import random_split
+from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.tensorboard import SummaryWriter
+import torch.multiprocessing as mp
+import torch.distributed as dist
+from sklearn.model_selection import KFold, train_test_split
+from collections import defaultdict
+
+from rrl.utils import read_csv, DBEncoder
+from rrl.models import RRL
+
+DATA_DIR = './dataset'
+
+
+def get_data(dataset, k=0):
+    data_path = os.path.join(DATA_DIR, dataset + '.data')
+    info_path = os.path.join(DATA_DIR, dataset + '.info')
+    X_df, y_df, f_df, label_pos = read_csv(data_path, info_path, shuffle=True)
+    
+    db_enc = DBEncoder(f_df, discrete=False)
+    db_enc.fit(X_df, y_df)
+
+    X, y = db_enc.transform(X_df, y_df, normalized=True, keep_stat=True)
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=0)
+    train_index, test_index = list(kf.split(X_df))[k]
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    return X_train, y_train, X_test, y_test
+
+from sklearn import tree
+def train_and_test_decision_tree_regression(dataset):
+    X_train, y_train, X_test, y_test = get_data(dataset)
+
+    model = tree.DecisionTreeRegressor()
+    model.fit(X_train, y_train)
+    result = model.predict(X_test)
+    
+    # add metrics calculation
+    print(np.mean(np.abs(y_test - result)))
+    return
+
+from sklearn.linear_model import Ridge
+def train_and_test_ridge_regression(dataset):
+    X_train, y_train, X_test, y_test = get_data(dataset)
+
+    model = Ridge(alpha=1)
+    model.fit(X_train, y_train)
+    result = model.predict(X_test)
+    
+    # add metrics calculation
+    print(np.mean(np.abs(y_test - result)))
+    return
+
+
+class MyRidge:
+    def __init__(self, alpha=1, n_iters=1000, lr=1e-4):
+        self.alpha = alpha
+        self.n_iters = n_iters
+        self.lr = lr
+
+    def fit(self, X, y):
+        X_ = np.insert(X, 0, 1, axis=1)
+        
+        # initialize
+        bound = (X_.shape[1])**-0.5
+        self.w = np.random.uniform(-bound, bound, (X_.shape[1],))
+
+        # regression
+        for i in range(self.n_iters):
+            y_pred = X_.dot(self.w)
+            self.w -= self.lr * ((y_pred - y).dot(X_) + self.alpha * self.w)
+    
+    def predict(self, X):
+        return np.insert(X, 0, 1, axis=1).dot(self.w)
+    
+
+def train_and_test_ridge_regression_raw(dataset):
+    X_train, y_train, X_test, y_test = get_data(dataset)
+
+    model = MyRidge(alpha=1)
+    model.fit(X_train, y_train)
+    result = model.predict(X_test)
+    
+    # add metrics calculation
+    print(np.mean(np.abs(y_test - result)))
+    return
+
+
+if __name__ == '__main__':
+    mode = sys.argv[1]
+    dataset = sys.argv[2]
+
+    assert mode in ['tree', 'ridge', 'ridge_raw'], 'Invalid mode!!'
+    assert dataset in ['WineQuality', 'BostonHousing', 'OnlineNewsPopularity'], 'Invalid dataset!!'
+    if mode == 'tree':
+        train_and_test_decision_tree_regression(dataset)
+    elif mode == 'ridge':
+        train_and_test_ridge_regression(dataset)
+    elif mode == 'ridge_raw':
+        train_and_test_ridge_regression_raw(dataset)
+    else:
+        raise NotImplementedError
+    
--- rrl/utils.py
+++ rrl/utils.py
@@ -48,7 +48,7 @@ class DBEncoder:
         continuous_data = X_df[self.f_df.loc[self.f_df[1] == 'continuous', 0]]
         if not continuous_data.empty:
             continuous_data = continuous_data.replace(to_replace=r'.*\?.*', value=np.nan, regex=True)
-            continuous_data = continuous_data.astype(np.float)
+            continuous_data = continuous_data.astype(np.float32)
         return discrete_data, continuous_data
 
     def fit(self, X_df, y_df):
@@ -56,7 +56,7 @@ class DBEncoder:
         y_df = y_df.reset_index(drop=True)
         discrete_data, continuous_data = self.split_data(X_df)
         self.label_enc.fit(y_df)
-        self.y_fname = list(self.label_enc.get_feature_names(y_df.columns)) if self.y_one_hot else y_df.columns
+        self.y_fname = list(self.label_enc.get_feature_names_out(y_df.columns)) if self.y_one_hot else y_df.columns
 
         if not continuous_data.empty:
             # Use mean as missing value for continuous columns if do not discretize them.
@@ -79,9 +79,10 @@ class DBEncoder:
         y_df = y_df.reset_index(drop=True)
         discrete_data, continuous_data = self.split_data(X_df)
         # Encode string value to int index.
-        y = self.label_enc.transform(y_df.values.reshape(-1, 1))
-        if self.y_one_hot:
-            y = y.toarray()
+        # y = self.label_enc.transform(y_df.values.reshape(-1, 1))
+        # if self.y_one_hot:
+        #     y = y.toarray()
+        y = y_df.values[:, 0].astype(np.float32)
 
         if not continuous_data.empty:
             # Use mean as missing value for continuous columns if we do not discretize them.
